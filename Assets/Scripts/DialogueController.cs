82fee0189f75eef9e659d8efe8cb90a35bb3ee15
Bart <bartvanlierop.bvl@gmail.com>
2021-04-08 15:03:53 +0200

    Gitingore gefixed

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+    private Queue<string> names;
+    private Queue<GameObject> icons;
+    private GameObject currIcon;
+
+    private bool doneOnce = false;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+        names = new Queue<string>();
+        icons = new Queue<GameObject>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+
+        sentences.Clear();
+        names.Clear();
+        icons.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        foreach (string name in dialogue.names)
+        {
+            names.Enqueue(name);
+        }
+        foreach (GameObject icon in dialogue.icons)
+        {
+            icons.Enqueue(icon);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        if (doneOnce)
+        {
+            currIcon.SetActive(false);
+        }
+
+        GameObject icon = icons.Dequeue();
+        string sentence = sentences.Dequeue();
+        string name = names.Dequeue();
+        StopAllCoroutines();
+        nameText.text = name;
+        icon.SetActive(true);
+        currIcon = icon;
+        StartCoroutine(TypeSentence(sentence));
+        doneOnce = true;
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach  (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    private float horizontalMove = 0f;
+    private float move;
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;
+
+    public enum StateEnum { playing, cutscene }
+    private StateEnum state;
+
+    private Vector3 velocity = Vector3.zero;
+
+    Rigidbody2D rigidbody2D;
+    Animator animator;
+    public DialogueTrigger dialogueTrigger;
+
+    private bool m_FacingRight = true;
+    public bool triggered = false;
+    public bool started = false;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {        
+        CheckState();
+    }
+
+    void CheckState()
+    {
+        switch (state)
+        {
+            case StateEnum.playing: PlayingBehaviour(); break;
+            case StateEnum.cutscene: CutsceneBehaviour(); break;
+        }
+    }
+
+    private void PlayingBehaviour()
+    {
+        checkInput();
+        movement();
+
+        if (triggered)
+        {
+            state = (StateEnum.cutscene);
+        }
+    }
+
+    private void CutsceneBehaviour()
+    {
+        checkInput();
+        StartCutscene();
+    }
+
+    void movement()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        move = horizontalMove * Time.fixedDeltaTime;
+        Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = Vector3.SmoothDamp(rigidbody2D.velocity, targetVelocity, ref velocity, movementSmoothing);
+
+        if (move > 0 && !m_FacingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && m_FacingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void checkInput()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("trigger");
+        triggered = true;
+    }
+
+    void StartCutscene()
+    {
+        if (!started)
+        {
+            dialogueTrigger.TriggerDialogue();
+            started = true;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
